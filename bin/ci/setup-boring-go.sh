54ce8d31f668ce745e82baeaa13b60a88838fd41
buyr-power <66888388+buyr-power@users.noreply.github.com>
2022-04-24 18:49:12 -0400

    Update setup-boring-go.sh
    Add prefix

--- bin/ci/setup-boring-go.sh
+++ bin/ci/setup-boring-go.sh
@@ -4,7 +4,7 @@ set -u
 
 url="https://go-boringcrypto.storage.googleapis.com/go$1.linux-amd64.tar.gz"
 
-wget -q -O go.tgz "$url"
+wget -q -O boring-go.tgz "$url"
 
 sudo tar -C /usr/local -xzf boring-go.tgz
 rm boring-go.tgz
